2fb39cbeaec043b617704f10f590ca1e1defa38a
swati <swati.padalia@dosetap.com>
2024-06-08 23:58:25 +0530

    dosetap-dev-portal-build stage

---
+++ precache-manifest.c7a1802223888d6fc8db963e5190c71b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fac9999a952a380955e9d4c76e8ef31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e141f0076b7bbae96eef",
+    "url": "/static/css/2.848ae295.chunk.css"
+  },
+  {
+    "revision": "721e4be5d200bee7750d",
+    "url": "/static/css/main.1c6aed73.chunk.css"
+  },
+  {
+    "revision": "e141f0076b7bbae96eef",
+    "url": "/static/js/2.4ff300d6.chunk.js"
+  },
+  {
+    "revision": "b20363b881544acdebf4ede24db28320",
+    "url": "/static/js/2.4ff300d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ebfa4143d244b04b67",
+    "url": "/static/js/3.93c8ece0.chunk.js"
+  },
+  {
+    "revision": "721e4be5d200bee7750d",
+    "url": "/static/js/main.c97c94cb.chunk.js"
+  },
+  {
+    "revision": "836c355977471ceb73f8",
+    "url": "/static/js/runtime-main.ab2e6aec.js"
+  }
+]);
